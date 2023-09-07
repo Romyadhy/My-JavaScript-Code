b269fa0ce7f5342c580c5f721a62fcdf8226f3aa
romyadhy <romyady221@gmail.com>
2023-09-07 08:33:49 +0800

    fixin

---
+++ 01_JavaScript/01.js
@@ -0,0 +1,14 @@
+document.writeln("Masih bingun rom???")
+
+/*Comentar untuk bacaan oanjang
+jang
+jang
+kang */
+
+//OKe jadi selanjutnya adalah Type Dta Number//
+/* Karena di javascript hanya ada 1 type data number saja. 
+Kita juga bisa menjalankan tipe data "Binary", "Hexadesimal", "Oktal"
+
+1. Binaray = 0b0110
+2. Hexadecimal = 0xA7
+3. Oktal = 0o10*/
---
+++ Loop/main.js
@@ -0,0 +1,31 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log('ini value ke- ', i)
+// }
+
+//Tugas
+/*1. Perulangan bertipe variabel array 
+contoh const x = [1, 2, 3, 4, 5]
+Tampilkan total nilai value diatas dalam for, do-while, while */ 
+
+// let total = 0;
+// let value = [100, 4, 5, 6, 4, 4, 5, 5, 6];
+
+// for(let i = 0; i < value.length; i++){
+//     total += value[i];
+// }
+// console.log('Jadi total valuenya adalah: ', total);
+
+//while
+// let x = 1
+// while (x <= 5) {
+//     console.log('data anda: ',x);
+//     x++
+// }
+
+let total = 0;
+let value = [1, 2, 3, 4, 5, 8]
+let indeks = 0
+while (value) {
+    total += value.length[indeks]
+    console.log('valuenya adalah: ', total)
+}
